4fca336bedb7aec4a6cc19433a4f4355e764f91f
Matt <mgstankov30@gmail.com>
2022-03-16 16:16:14 -0400

    refactor the JS code for the counter with help from @yani5752000

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
   $(".textarea").keyup( function () {
     let leftover = 140 - $(this).val().length 
+    const elem1 = $(this).parent();
+    const elem2 = $(elem1).children()[2];
+    const elem3 = $(elem2).children()[1];
     if (leftover >= 0) {
-    $(".textarea").parents("form").find("output.counter").html(leftover).css({"color": "black"});
+    $(elem3).html(leftover).css({"color":"black"});
     }
     if (leftover <= 0) {
-      $(".textarea").parents("form").find("output.counter").html(leftover).css({"color":"red"});
+      $(elem3).html(leftover).css({"color":"red"});
     }
   });
 });
