190f4a631a053a167f3837ece9a9e5c454ebd16a
Matt <mgstankov30@gmail.com>
2022-03-22 20:36:38 -0400

    add initial js call to run ajax to post a form submission on the submit call on the page, credit to @rachelpr

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,32 +1,33 @@
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 /* 
  * assisted by @nosaoasis and @rachelpr
@@ -51,7 +52,7 @@ const renderTweets = function(tweets) {
  * using template literals pass the dynamic values from those keys to the html element
  * return the markup
  */
-// $( document ).ready(function() {
+
 const createTweetElement = function(tweetObj) {
   const { user, content, created_at } = tweetObj;
   let markup = $(`
@@ -77,27 +78,25 @@ const createTweetElement = function(tweetObj) {
   return markup;
 };
 
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
+// renderTweets(data);
 
+$("#tweetpost").on("submit", function(event) {
+  event.preventDefault();
+  $.ajax({
+    method: "POST",
+    url: "/tweets",
+    data: $(this).serialize()
+  })
+  console.log($(this).serialize());
+});
 
-// const $tweet = createTweetElement(tweetData);
-
-// console.log($tweet); // to see what it looks like
-//   $("#tweet-container").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-// });
-renderTweets(data);
-
-// $( "button" ).click(function() {
-//   $( "#tweetpost" ).submit();
-//   event.preventDefault();
-// });
+$(document).ready(function() {
+  const loadTweets = function () {
+    $.ajax({
+      type: "GET",
+      data: "JSON",
+    }).then(function(data) {
+      renderTweets(data);
+    });
+  }
+});
